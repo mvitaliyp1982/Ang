bb89bf058774af43ac1d0c3e0258e01aadca2ffd
mvitaliyp1982 <https://github.com/mvitaliyp1982/Ang.git>
2023-01-22 13:52:14 +0200

    second comit

--- src/app/hw02/hw02.component.ts
+++ src/app/hw02/hw02.component.ts
@@ -6,8 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./hw02.component.css']
 })
 export class Hw02Component {
-  profile:Array<string> =["01.06.1982", "+380673722498", "mvitaliyp1982@gmail.com", "Хмельницький", "Мединський В.П."]
-  // number:string = "+380673722498"
-  work:Array<string> = [`МКП "Хмельницькводоканал" - 2004-2006`, `ТОВ "Карат" - 2006-по даний час`, `ще не придумав чим би похвалитись`]
-  social:Array<string> = ["https://www.facebook.com/profile.php?id=100006269657922", "https://www.instagram.com/vitalii_medynskyi/"]
+  profile:Array<string> =["01.06.1982", "+380673722498", "mvitaliyp1982@gmail.com", "Хмельницький", "Мединський Віталій Петрович"];
+  work:Array<string> = [`МКП "Хмельницькводоканал" - 2004-2006`, `ТОВ "Карат" - 2006-по даний час`, `ще не придумав чим би похвалитись`];
+  social:Array<string> = ["https://www.facebook.com/profile.php?id=100006269657922", "https://www.instagram.com/vitalii_medynskyi/"];
+  salary:number = 1000;
+  employment:Array<string> = [`часткова`, `1`];
 }
