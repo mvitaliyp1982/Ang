04d5c0a51e283556852a4a37797ac947c12b2be3
mvitaliyp1982 <https://github.com/mvitaliyp1982/Ang.git>
2023-01-30 13:04:09 +0200

    zzz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { BiggestCarComponent } from './biggest-car/biggest-car.component';
 import { BiggestShipComponent } from './biggest-ship/biggest-ship.component';
 import { BiggestCarAirplaneShipComponent } from './biggest-car-airplane-ship/biggest-car-airplane-ship.component';
 import { SwitchComponent } from './switch/switch.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { GallerySanFranciscoComponent } from './gallery-san-francisco/gallery-san-francisco.component';
 
 
 const routes: Routes = [
@@ -19,14 +21,26 @@ const routes: Routes = [
   children:[
     {path: 'Airplane', component: BiggestAirplaneComponent},
     {path: 'Car', component: BiggestCarComponent},
-    {path: 'Ship', component: BiggestShipComponent}
+    {path: 'Ship', component: BiggestShipComponent},
+    {path: 'GalleryComponent', component: GalleryComponent},
+    {path: 'SwitchComponent', component: SwitchComponent},
+    {path: 'GallerySanFranciscoComponent', component: GallerySanFranciscoComponent},
   ]
 },
-  {path:'StrangeHobbyComponent', component: StrangeHobbyComponent},
-  {path:'Hw02Component', component: Hw02Component},
-  {path:'Hw01Component', component: Hw01Component},
+    {path: 'StrangeHobbyComponent', component: StrangeHobbyComponent},
+    {path: 'Hw02Component', component: Hw02Component},
+    {path: 'Hw01Component', component: Hw01Component},
+    {path: 'GalleryComponent', component: GalleryComponent},
+    {path: 'GallerySanFranciscoComponent', component: GallerySanFranciscoComponent},   
 ]
 
+const routes2: Routes = [
+  {path:'', component: GalleryComponent,
+  children:[
+  {path: 'GallerySanFranciscoComponent', component: GallerySanFranciscoComponent},
+  ]
+}
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -39,6 +53,8 @@ const routes: Routes = [
     BiggestShipComponent,
     BiggestCarAirplaneShipComponent,
     SwitchComponent,
+    GalleryComponent,
+    GallerySanFranciscoComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/gallery-san-francisco/gallery-san-francisco.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-san-francisco',
+  templateUrl: './gallery-san-francisco.component.html',
+  styleUrls: ['./gallery-san-francisco.component.css']
+})
+export class GallerySanFranciscoComponent {
+  city:Array<string> = ['San Francisco', 'London', 'New York', 'Cape Town', 'Beijing', 'Paris']
+  src:Array<string> = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/san-fransisco-768x432.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/london-768x432.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/new-york-768x432.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/cape-town-768x432.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/beijing-768x432.jpg', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/paris-768x432.jpg']
+}
